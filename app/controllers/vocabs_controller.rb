521f2186b083890569fdd144c7d528d4d8af2cc3
Mashimo Hiroki <mc.mdt1007it@gmail.com>
2019-12-14 21:06:50 +0900

    #9 update vocab controller 同一単語入らない

--- app/controllers/vocabs_controller.rb
+++ app/controllers/vocabs_controller.rb
@@ -1,24 +1,29 @@
 class VocabsController < ApplicationController
 	def create
-		project_id = ENV["CLOUD_PROJECT_ID"]
-		translate   = Google::Cloud::Translate.new version: :v2, project_id: project_id
+		unless Vocab.find_by(user_article_id:params[:user_article_id], english: params[:english])
+			project_id = ENV["CLOUD_PROJECT_ID"]
+			translate   = Google::Cloud::Translate.new version: :v2, project_id: project_id
 
-	 	@english = params[:english]
+		 	@english = params[:english]
 
 
-	 	target = "ja"
+		 	target = "ja"
 
-		# Translates some text into Russian
-		@japanese = translate.translate @english, to: target
-	 	# @vocab = vocab.new(vocab_params)
-	 	# binding.pry
-	 	@vocab = Vocab.new
-	 	# @vocab.english = @english
-	 	@vocab.japanese = @japanese
-	 	@vocab.english = @english
-	 	@vocab.user_article_id = params[:user_article_id]
-	 	@vocab.save
-	 	redirect_to user_article_path(params[:user_article_id])
+			# Translates some text into Russian
+			@japanese = translate.translate @english, to: target
+		 	# @vocab = vocab.new(vocab_params)
+		 	# binding.pry
+		 	@vocab = Vocab.new
+		 	# @vocab.english = @english
+		 	@vocab.japanese = @japanese
+		 	@vocab.english = @english
+		 	@vocab.user_article_id = params[:user_article_id]
+	 	    @vocab.save
+	 	    redirect_to user_article_path(params[:user_article_id])
+	 	else
+	 	  	flash[:success] = "登録済の単語です"
+	 	    redirect_to user_article_path(params[:user_article_id])
+	 	end
 	end
 
 	def destroy
