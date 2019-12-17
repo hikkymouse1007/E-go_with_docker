5203e50a5127f778bd227ab84066257f83c81935
Mashimo Hiroki <mc.mdt1007it@gmail.com>
2019-12-17 16:23:18 +0900

    #39 migrate Publisher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_100908) do
+ActiveRecord::Schema.define(version: 2019_12_17_071241) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "url"
+    t.string "category"
+    t.string "language"
+    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
