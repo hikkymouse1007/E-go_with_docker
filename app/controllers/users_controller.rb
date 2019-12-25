7c5ef68f4a24f47a637071cf12a11c0404044a22
Mashimo Hiroki <mc.mdt1007it@gmail.com>
2019-12-25 18:42:39 +0900

    #64 finish issue #64

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update,:show,:destroy]
-  before_action :correct_user,   only: [:edit, :update,:show,:destroy]
+  before_action :logged_in_user, only: [:edit,:update,:words,:show,:destroy]
+  before_action :correct_user,   only: [:edit,:update,:words,:show,:destroy]
 
   def home
     @today = Date.today
